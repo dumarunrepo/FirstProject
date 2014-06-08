1bcd427dbc3aa68bd9eb41b7d7fa4676d8ef822c
unknown <607785843@BTG468233.iuser.iroot.adidom.com>
2014-06-08 22:03:28 +0530

    Bug Fix, DB script

--- DB_Schema/bootstrap_data_test.sql
+++ DB_Schema/bootstrap_data_test.sql
@@ -15,8 +15,8 @@ INSERT INTO project_stage(project_id,project_stage,project_status,report_url) va
 INSERT INTO project_stage(project_id,project_stage,project_status,report_url) values ('3','Testing','completed','facebook.com');
 INSERT INTO project_stage(project_id,project_stage,project_status,report_url) values ('4','Aditya vasectomy','progress','facebook.com');
 
-INSERT INTO users(user_id, user_clientname, user_fullname, user_email, 'user_password', user_status) values ('9590946168','Siva','Aditya Udupa','udupa.pappu@popat.com','somehashvalue','1');
-INSERT INTO users(user_id, user_clientname, user_fullname, user_email, 'user_password', user_status) values ('9901199420','Siva','Monish Kaul','kaulmonish@gmail.com','somehashvalue','1');
+INSERT INTO users(user_id, user_clientname, user_fullname, user_email, `user_password`, user_status) values ('9590946168','Siva','Aditya Udupa','udupa.pappu@popat.com','somehashvalue','1');
+INSERT INTO users(user_id, user_clientname, user_fullname, user_email, `user_password`, user_status) values ('9901199420','Siva','Monish Kaul','kaulmonish@gmail.com','somehashvalue','1');
 
 INSERT INTO clients(name, email, designation, organisation, phone_no, address) values('siva','ksiva@gmail.com','CEO','Dumarun','Challaghatta-Bangalore');
 
