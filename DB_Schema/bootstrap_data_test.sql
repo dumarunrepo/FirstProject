da37c9b229cfaba9acb136268fe23bf18af544e1
unknown <607785843@BTG468233.iuser.iroot.adidom.com>
2014-06-08 21:57:38 +0530

    DB scripts - formatting and bootstrap code.

---
+++ DB_Schema/bootstrap_data_test.sql
@@ -0,0 +1,24 @@
+INSERT INTO projects (project_id,project_name,is_completed,description, start_date) VALUES ('1','test_project_1', 'true', 'This is a test Description for Udupa testing', NOW());
+INSERT INTO projects (project_id,project_name,is_completed,description, start_date) VALUES ('2','test_project_2', 'true', 'This is a test Description for Udupa testing', NOW());
+INSERT INTO projects (project_id,project_name,is_completed,description, start_date) VALUES ('3','test_project_3', 'true', 'This is a test Description for Udupa testing', NOW());
+INSERT INTO projects (project_id,project_name,is_completed,description, start_date) VALUES ('4','test_project_4', 'true', 'This is a test Description for Udupa testing', NOW());
+
+INSERT INTO permission (project_id, user_id, permission_type) values('1','9590946168',1);
+INSERT INTO permission (project_id, user_id, permission_type) values('2','9590946168',1);
+INSERT INTO permission (project_id, user_id, permission_type) values('3','9590946168',1);
+INSERT INTO permission (project_id, user_id, permission_type) values('4','9590946168',1);
+INSERT INTO permission (project_id, user_id, permission_type) values('2','9901199420',1);
+INSERT INTO permission (project_id, user_id, permission_type) values('3','9901199420',1);
+
+INSERT INTO project_stage(project_id,project_stage,project_status,report_url) values ('1','RNA Analysis','progress','facebook.com');
+INSERT INTO project_stage(project_id,project_stage,project_status,report_url) values ('2','RNA Analysis','completed','facebook.com');
+INSERT INTO project_stage(project_id,project_stage,project_status,report_url) values ('3','Testing','completed','facebook.com');
+INSERT INTO project_stage(project_id,project_stage,project_status,report_url) values ('4','Aditya vasectomy','progress','facebook.com');
+
+INSERT INTO users(user_id, user_clientname, user_fullname, user_email, 'user_password', user_status) values ('9590946168','Siva','Aditya Udupa','udupa.pappu@popat.com','somehashvalue','1');
+INSERT INTO users(user_id, user_clientname, user_fullname, user_email, 'user_password', user_status) values ('9901199420','Siva','Monish Kaul','kaulmonish@gmail.com','somehashvalue','1');
+
+INSERT INTO clients(name, email, designation, organisation, phone_no, address) values('siva','ksiva@gmail.com','CEO','Dumarun','Challaghatta-Bangalore');
+
+INSERT INTO supports(email, contactNo) values ('support@bionivid.com', '9901199649');
+INSERT INTO sales(email, contactNo) values ('sales@bionivid.com', '9901199649');
--- DB_Schema/create_bionivid_basic_schema.sql
+++ DB_Schema/create_bionivid_basic_schema.sql
@@ -8,11 +8,6 @@ CREATE TABLE projects (
     end_date DATETIME DEFAULT NULL
 );
 
-INSERT INTO projects (project_id,project_name,is_completed,description, start_date) VALUES ('sdhdsk','monish_1', 'true', 'Description', NOW());
-INSERT INTO projects (project_id,project_name,is_completed,description, start_date) VALUES ('ffdsdf','monish_2', 'true', 'Description', NOW());
-INSERT INTO projects (project_id,project_name,is_completed,description, start_date) VALUES ('dshgkd','monish_3', 'true', 'Description', NOW());
-INSERT INTO projects (project_id,project_name,is_completed,description, start_date) VALUES ('dfasds','monish_4', 'true', 'Description', NOW());
-
 CREATE TABLE IF NOT EXISTS permission (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     project_id VARCHAR(50),
@@ -20,8 +15,6 @@ CREATE TABLE IF NOT EXISTS permission (
     permission_type int(1) DEFAULT 0,
     UNIQUE KEY (project_id, user_id)
 );
-
-insert into permission (project_id, user_id, permission_type) values('sdhdsk','5',1);
 
 CREATE TABLE IF NOT EXISTS project_stage (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -32,9 +25,6 @@ CREATE TABLE IF NOT EXISTS project_stage (
     end_date DATETIME DEFAULT NULL,
     report_url TEXT
 );
-
-INSERT INTO project_stage(project_id,project_stage,project_status,report_url) values ('sdhdsk','RNA Analysis','progress','url_1');
-INSERT INTO project_stage(project_id,project_stage,project_status,report_url) values ('sdhdsk','RNA Analysis','completed','url_2');
 
 CREATE TABLE IF NOT EXISTS `users`
 (
@@ -57,7 +47,6 @@ phone_no bigint,
 address TEXT
 );
 
-
 CREATE TABLE IF NOT EXISTS `feedbacks`
 (
 id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -73,7 +62,6 @@ id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 email varchar(50) NOT NULL DEFAULT 'support@bionivid.com',
 contactNo varchar(50) NOT NULL
 );
-insert into supports(email, contactNo) values ('support@bionivid.com', '9901199649');
 
 CREATE TABLE IF NOT EXISTS `sales`
 (
@@ -81,5 +69,4 @@ id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 email varchar(50) NOT NULL DEFAULT 'sales@bionivid.com',
 contactNo varchar(50) NOT NULL
 );
-insert into sales(email, contactNo) values ('sales@bionivid.com', '9901199649');
 
