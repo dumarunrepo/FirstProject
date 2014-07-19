179451469966f14c0e72de65cce4d54936c428f0
unknown <607785843@BTG468233.iuser.iroot.adidom.com>
2014-07-19 17:01:14 +0530

    db change + query change

--- Rest/cakephp-2.5.0/app/Controller/RestStatisticsController.php
+++ Rest/cakephp-2.5.0/app/Controller/RestStatisticsController.php
@@ -6,7 +6,7 @@ class RestStatisticsController extends AppController {
     public $components = array('RequestHandler');
 
     public function index() {
-	$Statistics = $this->Statistic->query('select count(1) as `statistics` from projects p union select  count(1) from clients union select count(1) from feedbacks;');
+	$Statistics = $this->Statistic->query('select (select count(1) from projects) as projects,(select count(1) from clients) as clients,(select count(1) from feedbacks) as feedbacks;');
 	$this->set(array(
             'Statistics' => $Statistics,
             '_serialize' => array('Statistics')
