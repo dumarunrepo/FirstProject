9d63ec4fb3ca09b81216dc2d5e77f6478f216a1d
unknown <607785843@BTG468233.iuser.iroot.adidom.com>
2014-07-29 18:08:59 +0530

    Latest changes suggested by vignesh anna

--- Rest/cakephp-2.5.0/app/Controller/RestNotificationsController.php
+++ Rest/cakephp-2.5.0/app/Controller/RestNotificationsController.php
@@ -5,8 +5,8 @@ class RestNotificationsController extends AppController {
     public $helpers = array('Html', 'Form');
     public $components = array('RequestHandler');
 
-    public function index() {
-	$Notifications = $this->Notification->find('all');
+    public function index($id) {
+	$Notifications = $this->Notification->query("select * from notifications where client_name in ('-1', '$id')");
 	$this->set(array(
             'Notifications' => $Notifications,
             '_serialize' => array('Notifications')
