63406b9ab17816534e18fc832aad48d710cc0171
unknown <607785843@BTG468233.iuser.iroot.adidom.com>
2014-08-03 20:54:25 +0530

    m

--- Rest/cakephp-2.5.0/app/Controller/RestProductsController.php
+++ Rest/cakephp-2.5.0/app/Controller/RestProductsController.php
@@ -6,7 +6,7 @@ class RestProductsController extends AppController {
     public $components = array('RequestHandler');
 
     public function index($id) {
-	$Products = $this->Product->query("select * from products where client_name in ('-1', '$id')");
+	$Products = $this->Product->query("select * from products where client_name in ('-1') or client_name=(select user_clientname from users where user_id='$id')");
 	$this->set(array(
             'Products' => $Products,
             '_serialize' => array('Products')
